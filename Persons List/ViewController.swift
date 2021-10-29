fdfaf17bedeeea9aa32d6db28b72814f43a97de9
Egor Astakhov <noise718@gmail.com>
2021-10-29 14:33:31 +0300

    Initial Commit

---
+++ Persons List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Persons List
+//
+//  Created by Егор Астахов on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
